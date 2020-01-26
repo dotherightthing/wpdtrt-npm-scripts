da1dc49dfc304c92080b05d656dfa4fb0b775990
Dan Smith <dev@dotherightthing.co.nz>
2020-01-26 14:29:34 +1300

    Move Natural Docs execution into a shell script

---
+++ scripts/docs/naturaldocs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# File: ./docs/naturaldocs.sh
+#
+# Note:
+# chmod a+x = Change access permissions of this file, to 'e[x]ecutable' for '[a]ll users'
+#
+# Example:
+# ---
+# chmod a+x scripts/**/*.sh
+# ---
+
+# e: exit the script if any statement returns a non-true return value
+# v: print shell input lines as they are read (including all comments!)
+set -e
+
+echo "Generate Natural Docs documentation" \
+&& cd $INIT_CWD \
+&& mono bin/Natural\ Docs/NaturalDocs.exe ./config/naturaldocs
