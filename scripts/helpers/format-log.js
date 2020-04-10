ca35edf36f72aec8fe8ff19316e0498a0a18f47a
Dan Smith <dev@dotherightthing.co.nz>
2020-04-11 11:13:15 +1200

    Add timestamp to log messages

--- scripts/helpers/format-log.js
+++ scripts/helpers/format-log.js
@@ -16,10 +16,11 @@ const log = (args) => {
 
     // first arg is path to nodejs
     // second arg is the location of the script being executed
+    const dateTime = `~ ${new Date().toLocaleString()} ~ `;
     const category = args[0] ? `${args[0]}` : '';
     const task = args[1] ? `: ${args[1]}` : '';
     const description = args[2] ? ` (${args[2]})` : '';
-    const msg = (`wpdtrt-npm-scripts - ${category}${task}${description}`).toLowerCase();
+    const msg = (`wpdtrt-npm-scripts ${dateTime}${category}${task}${description}`).toLowerCase();
 
     console.log(' ');
     console.log('--------------------------------------------------------------------------------');
