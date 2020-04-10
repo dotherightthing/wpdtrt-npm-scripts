6ed399591df56fc173fc85235438f0f16a754ad7
Dan Smith <dev@dotherightthing.co.nz>
2020-04-10 22:30:24 +1200

    Try require path without leading dot

--- scripts/postinstall/remaining-github-api-calls.js
+++ scripts/postinstall/remaining-github-api-calls.js
@@ -4,33 +4,37 @@
  * @return {Object} Rate Limit object
  */
 
-const ghRateLimit = require( 'gh-rate-limit' );
+const getRemainingGithubApiCalls = () => {
+    const ghRateLimit = require( 'gh-rate-limit' );
 
-console.log('cwd = ' + process.cwd());
-
-const formatLog = require('./format-log.js');
-
-const token = process.env.GH_TOKEN ? process.env.GH_TOKEN : '';
-let msg;
-
-if ( token !== '' ) {
-
-    ghRateLimit( {
-        token: token
-    } ).then(status => {
-        msg = `${status.core.remaining}/${status.core.limit} API calls remaining`;
+    // console.log('cwd = ' + process.cwd()); // /Users/dan/Websites/wpdtrt-dbth/node_modules/wpdtrt-npm-scripts
+    
+    const formatLog = require('/scripts/helpers/format-log.js');
+    
+    const token = process.env.GH_TOKEN ? process.env.GH_TOKEN : '';
+    let msg;
+    
+    if ( token !== '' ) {
+    
+        ghRateLimit( {
+            token: token
+        } ).then(status => {
+            msg = `${status.core.remaining}/${status.core.limit} API calls remaining`;
+            formatLog.log([
+                'postinstall',
+                'Github API rate limit check',
+                msg
+            ]);
+        } );
+    
+    } else {
+        msg = 'skipping - GH_TOKEN not found)';
         formatLog.log([
             'postinstall',
             'Github API rate limit check',
             msg
         ]);
-    } );
+    }
+};
 
-} else {
-    msg = 'skipping - GH_TOKEN not found)';
-    formatLog.log([
-        'postinstall',
-        'Github API rate limit check',
-        msg
-    ]);
-}
+getRemainingGithubApiCalls();
