0f8912566202b16dbdf972ee27bc7424ed037a2c
Dan Smith <dev@dotherightthing.co.nz>
2020-04-11 16:35:25 +1200

    Delete release folder after release, housekeeping

--- scripts/postrelease/delete-release-folder.sh
+++ scripts/postrelease/delete-release-folder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# File: ./scripts/postuninstall/dependencies.sh
+# File: ./scripts/postuninstall/config.sh
 #
 # Note:
 # chmod a+x = Change access permissions of install.sh, to 'e[x]ecutable' for '[a]ll users'
@@ -15,8 +15,6 @@
 # v: print shell input lines as they are read (including all comments!)
 set -e
 
-node scripts/helpers/format-log.js 'postuninstall' 'dependencies' 'remove development dependencies files from host repository' \
+node scripts/helpers/format-log.js 'postrelease' 'delete folder' \
 && cd $INIT_CWD \
-&& npm uninstall eslint-config-airbnb-base \
-&& npm uninstall eslint-plugin-chai-friendly \
-&& npm uninstall eslint-plugin-cypress
+&& rm -r release*
