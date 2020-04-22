a53b34c80c76ea2d1bd79efb77b8a0c5a8f129f8
Dan Smith <dev@dotherightthing.co.nz>
2020-04-22 22:55:21 +1200

    Bundle install-wp-tests.sh from wpdtrt-plugin-boilerplate

--- scripts/test/install-wpunit.js
+++ scripts/test/install-wpunit.js
@@ -4,28 +4,12 @@
  */
 
 const execa = require('execa');
-const fs = require('fs');
-const path = require('path');
-
-// if not loaded as a dependency
-// if (!fs.existsSync('../../../../composer.json')) {
-//     /* eslint-disable no-console */
-//     console.error('install-wpunit.js: composer.json not found, exiting');
-//     /* eslint-enable no-console */
-
-//     return;
-// }
-
-// path.resolve:
-// https://www.bennadel.com/blog/3244-non-module-file-paths-are-relative-to-the-working-directory-in-node-js.htm
 
 const formatLog = require('../helpers/format-log.js').default;
 const pluginName = process.cwd().split('/').pop();
 const pluginNameSafe = pluginName.replace(/-/g, '_');
 const dbName = `${pluginNameSafe}_wpunit_${Date.now()}`;
 const wpVersion = 'latest';
-let installerPath = `${path.resolve('../../')}/vendor/`;
-const composerJson = require(`${path.resolve('../../')}/composer.json`);
 
 formatLog([
     'test',
@@ -34,20 +18,7 @@ formatLog([
 ]);
 
 const installWpUnit = async function () {
-    if (composerJson.require) {
-        const dependencies = Object.keys(composerJson.require);
-
-        if (dependencies.includes('dotherightthing/wpdtrt-plugin-boilerplate')) {
-            installerPath += 'dotherightthing/wpdtrt-plugin-boilerplate/';
-        }
-    }
-
-    const shellScript = `${installerPath}bin/install-wp-tests.sh`;
-
-    if (!fs.existsSync(shellScript)) {
-        console.log(`${shellScript} does not exist.`); // eslint-disable-line no-console
-        console.log('Skipping..\n\n'); // eslint-disable-line no-console
-    }
+    const shellScript = 'install-wp-tests.sh';
 
     try {
         const { stdout, stderr } = await execa.commandSync(`bash ${shellScript} ${dbName} ${wpVersion}`, { shell: true });
