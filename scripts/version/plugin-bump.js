ae71e3b790797203cfe55ec9b1409a238116e44a
Dan Smith <dev@dotherightthing.co.nz>
2020-05-10 18:14:07 +1200

    Port versioning script from wpdtrt-gulp (fixes #10)

--- scripts/version/plugin-bump.js
+++ scripts/version/plugin-bump.js
@@ -1,7 +1,95 @@
-const wpdtrtPluginBump = require('gulp-wpdtrt-plugin-bump').wpdtrtPluginBump;
-const boilerplatePath = require('../helpers/boilerplate-path.js').boilerplatePath;
+/**
+ * @file version/version.js
+ * @summary Tasks to version files prior to a release.
+ */
+const execa = require('execa');
+const path = require('path');
+const wpdtrtPluginBump = require('gulp-wpdtrt-plugin-bump');
 
-wpdtrtPluginBump({
-    inputPathRoot: './',
-    inputPathBoilerplate: `./${boilerplatePath()}`
-});
+const formatLog = require('../helpers/format-log.js').default;
+const packageJson = require(`${path.resolve('../../')}/package.json`);
+
+// const wordpressChildTheme = packageJson.keywords.includes('wordpress-child-theme');
+const wordpressPlugin = packageJson.keywords.includes('wordpress-plugin');
+const wordpressPluginBoilerplate = packageJson.name === 'wpdtrt-plugin-boilerplate';
+const wordpressPluginBoilerplatePath = packageJson.keywords.includes('wordpress-plugin') ? 'vendor/dotherightthing/wpdtrt-plugin-boilerplate/' : '';
+
+/* eslint-disable no-console */
+
+/**
+ * @function autoloadUpdatedDependencies (3/3)
+ * @summary Regenerate the list of PHP classes to be autoloaded.
+ */
+async function autoloadUpdatedDependencies() {
+    formatLog([
+        'version',
+        'autoloadUpdatedDependencies',
+        'Regenerate the list of PHP classes to be autoloaded'
+    ]);
+
+    try {
+        const { stdout, stderr } = await execa.commandSync('composer dump-autoload --no-interaction');
+        console.log(stdout);
+        console.log(stderr);
+    } catch (error) {
+        console.error(error.stdout);
+    }
+}
+
+/**
+ * @function replaceVersions (2/3)
+ * @summary Replace version strings, using the version set in package.json.
+ */
+function replaceVersions() {
+    formatLog([
+        'version',
+        'replaceVersions',
+        'Replace version strings'
+    ]);
+
+    if (wordpressPlugin || wordpressPluginBoilerplate) {
+        try {
+            wpdtrtPluginBump({
+                inputPathRoot: '../../',
+                inputPathBoilerplate: `./${wordpressPluginBoilerplatePath}`
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    } else {
+        console.log('This repository is not a plugin.');
+        console.log('Skipping..\n\n');
+    }
+}
+
+/**
+ * @function updateDependencies (1/3)
+ * @summary Update the boilerplate dependency to the latest version.
+ * @description
+ * Note:
+ * - If wpdtrt-plugin-boilerplate is loaded as a dependency,
+ *   get the latest release of wpdtrt-plugin-boilerplate.
+ * - This has to run before replaceVersions,
+ *   so that the correct version information is available
+ */
+async function updateDependencies() {
+    formatLog([
+        'version',
+        'updateDependencies',
+        'Update Composer dependencies'
+    ]);
+
+    if (wordpressPluginBoilerplatePath.length) {
+        try {
+            const { stdout, stderr } = await execa.commandSync('composer update dotherightthing/wpdtrt-plugin-boilerplate --no-interaction --no-suggest');
+            console.log(stdout);
+            console.log(stderr);
+        } catch (error) {
+            console.error(error.stdout);
+        }
+    }
+}
+
+updateDependencies();
+replaceVersions();
+autoloadUpdatedDependencies();
