02fd1444e610708be8acd6c01c9becddd188bbe4
Dan Smith <dev@dotherightthing.co.nz>
2020-05-17 12:21:00 +1200

    Automate generation of release changelog from git commit history

---
+++ scripts/release/changelog.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# File: ./scripts/release/changelog.sh
+#
+# Note:
+# chmod a+x = Change access permissions of this file, to 'e[x]ecutable' for '[a]ll users'
+#
+# Example:
+# ---
+# chmod a+x scripts/**/*.sh
+# ---
+
+# e: exit the script if any statement returns a non-true return value
+# v: print shell input lines as they are read (including all comments!)
+set -e
+
+# generate changelog
+
+node scripts/helpers/format-log.js 'release' 'changelog' 'generate from commit history' \
+&& cd $INIT_CWD \
+&& git log --pretty="* [%h] %s (%ar)" --no-merges $(git describe --tags --abbrev=0 @^)..@ > changelog.txt
