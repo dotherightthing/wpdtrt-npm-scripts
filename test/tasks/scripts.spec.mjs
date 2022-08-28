f56d54202ca904f2285e2c1174484e54ee5394ab
Dan Smith <dev@dotherightthing.co.nz>
2022-08-28 14:21:22 +1200

    Try $env.CI rather than $CI

--- test/tasks/scripts.spec.mjs
+++ test/tasks/scripts.spec.mjs
@@ -132,8 +132,8 @@ describe('scripts', function () {
         npmConfig: 'package.json',
         release: 'release',
         scss: [],
-        wpUnitWordPress: `${$CI ? $RUNNER_TEMP : tmpdir}/wordpress`,
-        wpUnitWordPressTestLibrary: `${$CI ? $RUNNER_TEMP : tmpdir}/wordpress-tests-lib`
+        wpUnitWordPress: `${$env.CI ? $env.RUNNER_TEMP : tmpdir}/wordpress`,
+        wpUnitWordPressTestLibrary: `${$env.CI ? $env.RUNNER_TEMP : tmpdir}/wordpress-tests-lib`
     };
 
     const placeholders = [
